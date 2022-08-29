30b9efae0e2c84471a9d20bc494c9be23c43a6b8
Lambels <patrick.arvatu@yahoo.com>
2022-08-29 20:22:24 +0300

    layer abstraction

--- layer.go
+++ layer.go
@@ -2,15 +2,45 @@ package sinoname
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"sync"
 
 	"golang.org/x/sync/errgroup"
 )
 
+// Layer represents an abstraction for a layer in the processing pipeline.
+//
+// Producer -> Layer 1 -> Layer 2 -> Layer 3 -> Sink
+//
+// Each layer in the pipeline must implement the PumpOut method which is responsible for
+// consuming values from a channel (which will close and must be handeled), modifing the
+// value and then passing the value back out the returned channel.
+//
+// Producer -> Layer 1 -> Layer 2 -> Layer 3 -> Sink
+//
+// Here layer 1 consumes 1 value from the producer which automatically closes its channel after
+// the one value it sends, then layer 1 is responsible for handling the value it gets, sending
+// the value out (optional) and closing its out channel.
+//
+// A layer must close its out channel if:
+//
+// 1: The context is cancelled. This may be caused by the client or by and error through the
+// pipeline.
+//
+// 2: The upstream channel is closed, this means that the upstream layer signaled that its done
+// sending values, process all the messages, send them out and close the out channel.
+//
+// All processing inside the layer must be called in the *errgroup.Group passed in via:
+//
+//	// to close the pipeline return an error here.
+//	(*errgroup.Group).Go(func() error {})
+type Layer interface {
+	// PumpOut handles reads messages from the channel, processes them and sends them through
+	// the out channel.
+	PumpOut(context.Context, *errgroup.Group, <-chan string) (<-chan string, error)
+}
+
 // Layers is an abstraction type for multiple layers.
-type Layers []*Layer
+type Layers []Layer
 
 // Run runs all the layers it owns returning a channel to read from and a cleanup function
 // which must be called in order to free all resources.
@@ -56,7 +86,7 @@ func (s Layers) Run(ctx context.Context, in string) (<-chan string, func() error
 	var lastOutC <-chan string
 	lastOutC = fanOutC
 	for _, layer := range s {
-		lastOutC, err = layer.Run(ctx, g, lastOutC)
+		lastOutC, err = layer.PumpOut(ctx, g, lastOutC)
 		if err != nil {
 			return nil, clnUp, err
 		}
@@ -66,107 +96,16 @@ func (s Layers) Run(ctx context.Context, in string) (<-chan string, func() error
 	return fanInC, clnUp, nil
 }
 
-// Layer holds all the transformers belonging to it, when the layer runs it fans out all the
-// messages it gets to all the transformers it owns.
-//
-// teoretically 1 message to a layer with 4 transformers results in 4 messages. 1 * 4
-// the output formula of each layer is sum(messages from up stream layer) * len(transformers)
-type Layer struct {
-	// cfg points to the config it belongs to.
-	// used to access meta data.
-	cfg *Config
-
-	// transformers is the transformers which get run for each message from the upstream channel.
-	transformers []Transformer
-}
-
-// Run recieves messages from the upstream layer via the in channel and passes them through the transformers.
-// The end products of the transformers are fed in the returned channel.
-func (s *Layer) Run(ctx context.Context, g *errgroup.Group, in <-chan string) (<-chan string, error) {
-	outC := make(chan string, s.cfg.MaxVals)
-
-	if len(s.transformers) == 0 {
-		return nil, errors.New("sinoname: layer has no transformers")
-	}
-
-	// wg is used to monitor the local go routines of this layer.
-	var wg sync.WaitGroup
-	pumpOut := func(ctx context.Context, t Transformer, v string) func() error {
-		f := func() error {
-			defer wg.Done()
-			val, err := t.Transform(v)
-			if err != nil {
-				return err
-			}
-
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case outC <- val:
-				return nil
-			}
-		}
-
-		return f
-	}
-
-	go func() {
-		// before the factory go-routine exits, either by a context cancelation or by the
-		// upstream's out channel closure, close the layers out channel.
-		defer func() {
-			defer close(outC)
-			// wait for all the transformers to send their value before closing.
-			wg.Wait()
-		}()
-
-		for {
-			select {
-			// ctx close signals a deliberate close or that an error occured somewhere
-			// throughout the pipeline, eitherway stop layer.
-			case <-ctx.Done():
-				return
-			case v, ok := <-in:
-				if !ok {
-					return
-				}
-
-				wg.Add(len(s.transformers))
-				for _, t := range s.transformers {
-					g.Go(
-						pumpOut(ctx, t, v),
-					)
-				}
-			}
-		}
-	}()
-
-	return outC, nil
-}
-
 // LayerFactory creates a new layer with the given config.
-type LayerFactory func(cfg *Config) *Layer
+type LayerFactory func(cfg *Config) Layer
 
-// Pack merges n layers into one singel layer, when a message is recieved by the layer it gets
+// Pack merges n transformer or proxy layers into one singel layer, when a message is recieved by the layer it gets
 // distributed to all the transformers it has.
 //
-// WARNING: Pack panics if it gets called with a Layer which has multiple transformers, the
-// max number of transformers per layer passed to pack is 1.
+// WARNING: pack returns nil when called with different types of layers, it only works with transformer
+// layers or proxy layers.
 func Pack(layerFacts ...LayerFactory) LayerFactory {
-	return func(cfg *Config) *Layer {
-		layer := &Layer{
-			cfg:          cfg,
-			transformers: make([]Transformer, len(layerFacts)),
-		}
+	return func(cfg *Config) Layer {
 
-		// populate new layer with transformers of other layers.
-		for i, layerFact := range layerFacts {
-			l := layerFact(cfg)
-			if len(l.transformers) != 1 {
-				panic("sinoname: unexpected number of transformers in layer")
-			}
-			layer.transformers[i] = l.transformers[0]
-		}
-
-		return layer
 	}
 }
