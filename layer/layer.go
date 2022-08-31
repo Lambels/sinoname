433aa56cb8e4acca8bc98457d20c4de7aff984a3
Lambels <patrick.arvatu@yahoo.com>
2022-08-31 19:48:30 +0300

    layer package

---
+++ layer/layer.go
@@ -0,0 +1,108 @@
+package layer
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Lambels/sinoname/config"
+	"github.com/Lambels/sinoname/transformer"
+	"golang.org/x/sync/errgroup"
+)
+
+// Layer represents an abstraction for a layer in the processing pipeline.
+//
+// Producer -> Layer 1 -> Layer 2 -> Layer 3 -> Sink
+//
+// Each layer in the pipeline must implement the PumpOut method which is responsible for
+// consuming values from a channel (which will close and must be handeled), modifing the
+// value and then passing the value back out the returned channel.
+//
+// Producer -> Layer 1 -> Layer 2 -> Layer 3 -> Sink
+//
+// Here layer 1 consumes 1 value from the producer which automatically closes its channel after
+// the one value it sends, then layer 1 is responsible for handling the value it gets, sending
+// the value out (optional) and closing its out channel.
+//
+// A layer must close its out channel if:
+//
+// 1: The context is cancelled. This may be caused by the client or by and error through the
+// pipeline.
+//
+// 2: The upstream channel is closed, this means that the upstream layer signaled that its done
+// sending values, process all the messages, send them out and close the out channel.
+//
+// All processing inside the layer must be called in the *errgroup.Group passed in via:
+//
+//	// to close the pipeline return an error here.
+//	(*errgroup.Group).Go(func() error {})
+type Layer interface {
+	// PumpOut handles reads messages from the channel, processes them and sends them through
+	// the out channel.
+	PumpOut(context.Context, *errgroup.Group, <-chan string) (<-chan string, error)
+}
+
+// Layers is an abstraction type for multiple layers.
+type Layers []Layer
+
+// Run runs all the layers it owns returning a channel to read from and a cleanup function
+// which must be called in order to free all resources.
+func (s Layers) Run(ctx context.Context, in string) (<-chan string, func() error, error) {
+	if len(s) == 0 {
+		return nil, func() error { return nil }, fmt.Errorf("sinoname: generator has no layers")
+	}
+
+	// fanOutC is used to fanOut messages to the first layer.
+	fanOutC := make(chan string, 1)
+	fanOutC <- in
+	close(fanOutC)
+
+	// the errgroup is used to stop all layers once one of the layers
+	// encounters an error from the source, rendering the source unreliable.
+	g, ctx := errgroup.WithContext(ctx)
+
+	// cancel is used to cancel the layers when the generator is done reading messages from the
+	// pipeline freeing the go-routines.
+	ctx, cancel := context.WithCancel(ctx)
+
+	// fanInC is used to fanIn all the messages from the last layer for the generator to consume.
+	//
+	// it is closed when either the context is cancelled by an error or explicit cancelation by the client
+	// or generator.
+	var fanInC <-chan string
+
+	// clnUp frees all go-routines created by the layers, not calling this function can cause
+	// a memory leak.
+	clnUp := func() error {
+		// cancel the child context, may already be cancelled (error ocurred or layers finished)
+		cancel()
+		// wait for layer go-routines to exit.
+		err := g.Wait()
+		if err == context.Canceled {
+			return nil
+		}
+		return err
+	}
+
+	// start layers pipeline.
+	var err error
+	var lastOutC <-chan string
+	lastOutC = fanOutC
+	for _, layer := range s {
+		lastOutC, err = layer.PumpOut(ctx, g, lastOutC)
+		if err != nil {
+			return nil, clnUp, err
+		}
+	}
+	fanInC = lastOutC
+
+	return fanInC, clnUp, nil
+}
+
+// LayerFactory takes in a config object and returns a new layer.
+type LayerFactory func(cfg *config.Config) Layer
+
+// TransformerFactory takes in a config object and returns a transformer.
+type TransformerFactory func(cfg *config.Config) transformer.Transformer
+
+// ProxyFactory takes in a config object and returns a proxy function.
+type ProxyFactory func(cfg *config.Config) ProxyFunc
---
+++ layer/layer_proxy.go
@@ -0,0 +1,87 @@
+package layer
+
+import (
+	"context"
+	"errors"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// ErrQuit can be returned from a proxy function to indicate that the whole generation process
+// should be stopped.
+//
+// No message will be recieved if this error is returned.
+var ErrQuit error = errors.New("sinoname: abort process")
+
+// ProxyFunc takes in a message and returns an error based on the input.
+//
+// If the error is not nil: the input is skipped.
+//
+// If the error is ErrQuit: the whole generation process gets closed.
+//
+// If the error is nil: the message is passed to further proxy functions.
+type ProxyFunc func(string) error
+
+// ProxyLayer holds all the proxy funcs it has, for a message to pass a proxy layer it must run
+// through all the proxys without any error.
+type ProxyLayer struct {
+	Proxys []ProxyFunc
+}
+
+// PumpOut recieves messages from the upstream layer via the in channel and passes them through the transformers.
+// The end products of the transformers are fed in the returned channel.
+func (l *ProxyLayer) PumpOut(ctx context.Context, g *errgroup.Group, in <-chan string) (<-chan string, error) {
+	outC := make(chan string)
+
+	if len(l.Proxys) == 0 {
+		return nil, errors.New("sinoname: layer has no proxys")
+	}
+
+	var wg sync.WaitGroup
+	pumpOut := func(ctx context.Context, in string) func() error {
+		f := func() error {
+			defer wg.Done()
+
+			for _, p := range l.Proxys {
+				if err := p(in); err != nil {
+					// if err is ErrQuit abort process.
+					if err == ErrQuit {
+						return err
+					}
+
+					// just return
+					return nil
+				}
+			}
+
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case outC <- in:
+				return nil
+			}
+		}
+
+		return f
+	}
+
+	go func() {
+		defer close(outC)
+
+		for {
+			select {
+			case <-ctx.Done():
+
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+
+				g.Go(pumpOut(ctx, v))
+			}
+		}
+	}()
+
+	return outC, nil
+}
