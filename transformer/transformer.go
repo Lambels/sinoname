3f7193f4bb9378f597d4468e10b7d82a1f1cc508
Lambels <patrick.arvatu@yahoo.com>
2022-09-09 18:14:50 +0300

    Added transform with signal

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -3,3 +3,22 @@ package transformer
 type Transformer interface {
 	Transform(in string) (string, error)
 }
+
+type Signal struct {
+	Err error
+	Val string
+}
+
+func TransformWithSignal(t Transformer, val string) <-chan Signal {
+	ch := make(chan Signal)
+
+	go func() {
+		val, err := t.Transform(val)
+		ch <- Signal{
+			Err: err,
+			Val: val,
+		}
+	}()
+
+	return ch
+}
