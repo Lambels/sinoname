d7f92405804e2d666bc9357d53ab5122a8a94aa3
Lambels <patrick.arvatu@yahoo.com>
2022-09-23 21:14:14 +0300

    updated test functions

--- layer/layer_test.go
+++ layer/layer_test.go
@@ -1,9 +1,28 @@
 package layer
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+type timeoutTransformer struct {
+	add string
+	d   time.Duration
+}
+
+func (t timeoutTransformer) Transform(ctx context.Context, val string) (string, error) {
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+
+	case <-time.After(t.d):
+		return val + t.add, nil
+	}
+}
 
 type errTransformer struct{}
 
-func (t errTransformer) Transform(string) (string, error) {
+func (t errTransformer) Transform(context.Context, string) (string, error) {
 	return "", errors.New("test error")
 }
