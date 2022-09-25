cb595a50cb9152f8eed6682d6907991182801895
Lambels <patrick.arvatu@yahoo.com>
2022-09-25 09:43:26 +0300

    simplified helper functions

--- helper/helper.go
+++ helper/helper.go
@@ -1,29 +1,12 @@
 package helper
 
 import (
-	"math/rand"
 	"strings"
-	"time"
 )
 
-type ReplaceMap map[rune][]rune
-
-func ReplaceRunes(in string, compare ReplaceMap) string {
-	r := rand.New(
-		rand.NewSource(time.Now().Unix()),
-	)
-
-	out := []rune(in)
-	for i, v := range []rune(in) {
-		if vals, ok := compare[v]; ok {
-			out[i] = vals[r.Intn(len(vals))]
-		}
-	}
-
-	return string(out)
-}
-
-func SplitOnSpecial(in string) string {
+func SplitOnSpecial(in string) []string {
 	r := strings.NewReplacer(".", " ", "_", " ", "-", " ")
-	return r.Replace(strings.TrimSpace(in))
+	v := r.Replace(strings.TrimSpace(in))
+
+	return strings.Fields(v)
 }
