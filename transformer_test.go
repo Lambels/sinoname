f32c67df4dbc6f8259f65c6b4351ab38ce4b2025
Lambels <patrick.arvatu@yahoo.com>
2022-10-20 16:44:46 +0300

    added tests

--- transformer_test.go
+++ transformer_test.go
@@ -31,6 +31,7 @@ func (n noopSource) Valid(context.Context, string) (bool, error) {
 
 func TestTransformer(t *testing.T) {
 	type testCase struct {
+		ctx     context.Context
 		t       TransformerFactory
 		in, out string
 	}
@@ -38,18 +39,25 @@ func TestTransformer(t *testing.T) {
 	var testCases []testCase
 
 	testCases = append(testCases,
-		// string cases
-		testCase{CamelCase, "-.camel -case test", "CamelCaseTest"},
-		testCase{KebabCase, "-.kebab -case test", "kebab-case-test"},
-		testCase{SnakeCase, "-.snake -case test", "snake_case_test"},
+		testCase{t: CamelCase, in: "-.camel -case test", out: "CamelCaseTest"},
+		testCase{t: KebabCase, in: "-.kebab -case test", out: "kebab-case-test"},
+		testCase{t: SnakeCase, in: "-.snake -case test", out: "snake_case_test"},
 
-		testCase{Plural, "plural test", "plural tests"},
-		testCase{SymbolTransformer(".", 1), "ABC", ".ABC"},
+		testCase{t: Plural, in: "plural test", out: "plural tests"},
+		testCase{t: SymbolTransformer(".", 1), in: "ABC", out: ".ABC"},
+		testCase{ContextWithNumber(context.Background(), 100), NumbersPrefix("_"), "Patrick1234", "100_Patrick1234"},
+		testCase{t: NumbersPrefix("-"), in: "Patrick1234", out: "1234-Patrick"},
 	)
 
+	// evaluate test cases.
 	for _, tc := range testCases {
+		if tc.ctx == nil {
+			tc.ctx = context.Background()
+		}
+
 		tr, _ := tc.t(testConfig)
-		out, err := tr.Transform(context.Background(), tc.in)
+
+		out, err := tr.Transform(tc.ctx, tc.in)
 		if err != nil {
 			t.Fatal(err)
 		}
