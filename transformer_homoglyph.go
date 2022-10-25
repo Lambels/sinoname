32ccabae9da279b27f22826993100af76b12df58
Lambels <patrick.arvatu@yahoo.com>
2022-10-25 20:21:13 +0300

    changed buf type + not done

--- transformer_homoglyph.go
+++ transformer_homoglyph.go
@@ -2,8 +2,8 @@ package sinoname
 
 import (
 	"context"
-	"strings"
 	"unicode/utf8"
+	"unsafe"
 )
 
 type ConfidenceMap struct {
@@ -81,87 +81,81 @@ type homoglyphTransformer struct {
 }
 
 func (t *homoglyphTransformer) Transform(ctx context.Context, in string) (string, error) {
+	if len(in)+utf8.UTFMax > t.maxLen {
+		return in, nil
+	}
+
 	var maxConfidence int
 	for _, v := range t.homoglyphs {
 		maxConfidence += v.MaxConfidence
 	}
 
-	// see strings.Map() .
-	for confidence := 0; confidence <= maxConfidence; confidence++ {
+	var buf []byte
+	var next string
+	// first iteration (confidence 0)
+	// do not allocate buffer if string remains unchanged.
+	for i, c := range in {
+		r := t.getRune(c, 0)
+		if r == c && c != utf8.RuneError {
+			continue
+		}
+
+		var width int
+		if c == utf8.RuneError {
+			c, width = utf8.DecodeRuneInString(in[i:])
+			// intended RuneError val.
+			if width != 1 && r == c {
+				continue
+			}
+		} else {
+			width = utf8.RuneLen(r)
+		}
+
+		// changed rune, allocate buffer.
+		buf = make([]byte, 0, len(in)+utf8.UTFMax)
+		buf = append(buf, in[:i]...)
+
+		if uint32(r) < utf8.RuneSelf {
+			buf = append(buf, byte(r))
+		} else {
+			utf8.EncodeRune(buf, r)
+		}
+
+		next = in[i+width:]
+		break
+	}
+
+	// capacity is 0. no buffer was initialized therefor value unchanged.
+	if cap(buf) == 0 {
+		return in, nil
+	}
+
+	// buffer initialized, add all values.
+	for i, c := range next {
+		r := t.getRune(c, 0)
+
+	}
+
+	out := bytesToString(buf)
+	ok, err := t.source.Valid(ctx, out)
+	if err != nil {
+		return "", err
+	}
+	if ok {
+		return in, nil
+	}
+
+	for confidence := 1; confidence <= maxConfidence; confidence++ {
 		select {
 		case <-ctx.Done():
 			return "", ctx.Err()
 		default:
 		}
 
-		var b strings.Builder
-		for i, c := range in {
-			r := t.getRune(c, confidence)
-			// same rune, no possible encoding error.
-			if r == c && c != utf8.RuneError {
-				continue
-			}
-
-			var width int
-			if c == utf8.RuneError {
-				c, width = utf8.DecodeRuneInString(in[i:])
-				// intended RuneError val.
-				if width != 1 && r == c {
-					continue
-				}
-			} else {
-				width = utf8.RuneLen(r)
-			}
-
-			b.Grow(len(in) + utf8.MaxRune)
-			b.WriteString(in[:i])
-			// write modified rune
-			if r >= 0 {
-				b.WriteRune(r)
-			}
-
-			in = in[i+width:]
-			break
-		}
-
-		if b.Cap() == 0 {
-			return in, nil
-		}
-
-		if next, err := t.sendIfValid(ctx, b.String()); !next || err != nil {
-			return in, err
-		}
-
-		// already allocated buffer, write all.
-		for _, c := range in {
-			r := t.getRune(c, confidence)
-
-			if r >= 0 {
-				// ASCII: write bytes (faster)
-				if r < utf8.RuneSelf {
-					b.WriteByte(byte(r))
-				} else {
-					b.WriteRune(r)
-				}
-			}
-
-			// try to send as sonn as possible.
-			if next, err := t.sendIfValid(ctx, b.String()); !next || err != nil {
-				return in, err
-			}
-		}
+		var i int
 	}
 
 	return in, nil
-}
-
-//TODO: return signals on how to proceed the loop.
-func (t *homoglyphTransformer) sendIfValid(ctx context.Context, in string) (bool, error) {
-	ok, err := t.source.Valid(ctx, in)
-	if err != nil {
-		return false, err
-	}
-
 }
 
 // replace replaces the rune at index i with a mapped runed at a given confidence.
@@ -185,4 +179,8 @@ func (t *homoglyphTransformer) getRune(c rune, confidence int) rune {
 	}
 
 	return c
+}
+
+func bytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
