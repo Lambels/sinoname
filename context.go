a8582eaca5846af99be1f7e3a68736c7db83e92a
Lambels <patrick.arvatu@yahoo.com>
2022-11-28 19:30:18 +0200

    docs

--- context.go
+++ context.go
@@ -4,10 +4,13 @@ import "context"
 
 type numKey struct{}
 
+// ContextWithNumber adds a number value to the context, this value will be linked to the
+// value you are sending in the pipeline to generate more custom outcomes.
 func ContextWithNumber(ctx context.Context, n int) context.Context {
 	return context.WithValue(ctx, numKey{}, n)
 }
 
+// NumberFromContext gets the number value from the context.
 func NumberFromContext(ctx context.Context) (int, bool) {
 	n, ok := ctx.Value(numKey{}).(int)
 	return n, ok
@@ -15,10 +18,13 @@ func NumberFromContext(ctx context.Context) (int, bool) {
 
 type stringKey struct{}
 
+// ContextWithString adds a string value to the context, this value will be linked to the
+// value you are sending in the pipeline to generate more custom outcomes.
 func ContextWithString(ctx context.Context, v stringKey) context.Context {
 	return context.WithValue(ctx, stringKey{}, v)
 }
 
+// StringFromContext gets the string value from the context.
 func StringFromContext(ctx context.Context) (string, bool) {
 	v, ok := ctx.Value(stringKey{}).(string)
 	return v, ok
@@ -26,6 +32,10 @@ func StringFromContext(ctx context.Context) (string, bool) {
 
 type sinkKey struct{}
 
+// SinkFromContext gets the sink channel from the context value.
+//
+// You can use this function to get the make messages skip the pipeline in certain points when
+// paired with ErrSkip.
 func SinkFromContext(ctx context.Context) (chan<- string, bool) {
 	v, ok := ctx.Value(sinkKey{}).(chan string)
 	return v, ok
