8ed50e9d532a87bb40eb359d97600b9f49052de2
Lambels <patrick.arvatu@yahoo.com>
2022-11-28 18:24:20 +0200

    fixed unicode bug + added all param support

--- transformer_abreviation.go
+++ transformer_abreviation.go
@@ -4,34 +4,42 @@ import (
 	"context"
 	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
-//TODO: add all option -> FOO BAR BUZ -> FBBUZ // FBARBUZ
-//TODO: all option for circumfix -> FOO BAR BUZ -> FBB
-
-var AbreviationPrefix = func(cfg *Config) (Transformer, bool) {
-	return &abreviationTransformer{
-		cfg:   cfg,
-		where: prefix,
-	}, false
+var AbreviationPrefix = func(all bool) func(cfg *Config) (Transformer, bool) {
+	return func(cfg *Config) (Transformer, bool) {
+		return &abreviationTransformer{
+			cfg:   cfg,
+			all:   all,
+			where: prefix,
+		}, false
+	}
 }
 
-var AbreviationSuffix = func(cfg *Config) (Transformer, bool) {
-	return &abreviationTransformer{
-		cfg:   cfg,
-		where: suffix,
-	}, false
+var AbreviationSuffix = func(all bool) func(cfg *Config) (Transformer, bool) {
+	return func(cfg *Config) (Transformer, bool) {
+		return &abreviationTransformer{
+			cfg:   cfg,
+			all:   all,
+			where: suffix,
+		}, false
+	}
 }
 
-var AbreviationCircumfix = func(cfg *Config) (Transformer, bool) {
-	return &abreviationTransformer{
-		cfg:   cfg,
-		where: circumfix,
-	}, false
+var AbreviationCircumfix = func(all bool) func(cfg *Config) (Transformer, bool) {
+	return func(cfg *Config) (Transformer, bool) {
+		return &abreviationTransformer{
+			cfg:   cfg,
+			all:   all,
+			where: circumfix,
+		}, false
+	}
 }
 
 type abreviationTransformer struct {
 	cfg   *Config
+	all   bool
 	where affix
 }
 
@@ -41,30 +49,40 @@ func (t *abreviationTransformer) Transform(ctx context.Context, in string) (stri
 	}
 
 	split := SplitOnSpecial(in, t.cfg.SplitOn)
+	lastX := len(split) - 1
 	var out string
 
 	switch t.where {
 	case prefix:
-		split[0] = string(unicode.ToUpper(rune(split[0][0])))
+		split[0] = ucCapital(split[0])
+
+		if t.all {
+			for i := 1; i < lastX; i++ {
+				split[i] = ucCapital(split[i])
+			}
+		}
 		out = strings.Join(split, "")
 
 	case suffix:
-		lastX := len(split) - 1
 
-		split[lastX] = string(unicode.ToUpper(rune(
-			split[lastX][len(split[lastX])-1],
-		)))
+		split[lastX] = ucCapital(split[lastX])
+
+		if t.all {
+			for i := lastX - 1; i > 0; i-- {
+				split[i] = ucCapital(split[i])
+			}
+		}
 		out = strings.Join(split, "")
 
 	case circumfix:
-		for i := 1; i < len(split)-1; i++ {
-			split[i] = string(unicode.ToUpper(rune(split[i][0])))
+		for i := 1; i < lastX; i++ {
+			split[i] = ucCapital(split[i])
+		}
+
+		if t.all {
+			split[0], split[lastX] = ucCapital(split[0]), ucCapital(split[lastX])
 		}
 		out = strings.Join(split, "")
-	}
-
-	if len(out) > t.cfg.MaxLen {
-		return in, nil
 	}
 
 	ok, err := t.cfg.Source.Valid(ctx, out)
@@ -73,4 +91,11 @@ func (t *abreviationTransformer) Transform(ctx context.Context, in string) (stri
 	}
 
 	return in, nil
+}
+
+func ucCapital(val string) string {
+	r, _ := utf8.DecodeRuneInString(val)
+	r = unicode.ToUpper(r)
+
+	return string(r)
 }
