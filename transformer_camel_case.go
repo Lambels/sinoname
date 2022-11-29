9dd6e23f2716154d33f00ed5f39dcaebdc0bb0e8
Lambels <patrick.arvatu@yahoo.com>
2022-11-29 18:31:42 +0200

    implemented camel case

---
+++ transformer_camel_case.go
@@ -0,0 +1,37 @@
+package sinoname
+
+import (
+	"context"
+	"strings"
+)
+
+// CamelCase splits on special characters and joins them back with capitalization.
+//
+// foo.bar_buz -> fooBarBuz
+var CamelCase = func(cfg *Config) (Transformer, bool) {
+	return &camelCaseTransformer{
+		cfg: cfg,
+	}, false
+}
+
+type camelCaseTransformer struct {
+	cfg *Config
+}
+
+func (t *camelCaseTransformer) Transform(ctx context.Context, in string) (string, error) {
+	if len(in) > t.cfg.MaxLen {
+		return in, nil
+	}
+
+	split := SplitOnSpecial(in)
+	for i := 1; i < len(split); i++ {
+		split[i] = ucCapitalFirst(split[i])
+	}
+
+	out := strings.Join(split, "")
+	if ok, err := t.cfg.Source.Valid(ctx, out); !ok || err != nil {
+		return in, err
+	}
+
+	return out, nil
+}
