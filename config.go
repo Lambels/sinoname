998488ec96f279f5df3753bb62ca8b4fe37aae53
Lambels <patrick.arvatu@yahoo.com>
2022-12-09 18:47:12 +0200

    added max bytes option

--- config.go
+++ config.go
@@ -7,10 +7,10 @@ import (
 
 // Config represents a config object accepted by sinoname.New(), sinoname.LayerFactor and sinoname.TransformerFactory .
 type Config struct {
-	// MaxLen is used to set the max length of the returned values (in bytes),
+	// MaxBytes is used to set the max length of the returned values (in bytes),
 	// it must be enforced by the transformers to make sure that they dont return
-	// values longer then MaxLen
-	MaxLen int
+	// values longer then MaxBytes
+	MaxBytes int
 
 	// MaxVals is used to set the max number of returned values.
 	// The consumer reads up to MaxVals values.
