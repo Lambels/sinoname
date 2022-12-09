ee70ec86277da27522c6684f456ff24792cdd8d6
Lambels <patrick.arvatu@yahoo.com>
2022-12-09 19:01:55 +0200

    message packet

---
+++ message.go
@@ -0,0 +1,21 @@
+package sinoname
+
+import "fmt"
+
+type MessagePacket struct {
+	// Copy of the actuall message.
+	Message string
+	// Number of changes the message has gone through.
+	Changes int
+	// Skip indicates a countdown on how many layers the message has to skip.
+	Skip int
+}
+
+func (m MessagePacket) String() string {
+	return fmt.Sprintf("%v , Changes: %v , Skips: %v", m.Message, m.Changes, m.Skip)
+}
+
+func (m *MessagePacket) setAndIncrement(v string) {
+	m.Message = v
+	m.Changes++
+}
